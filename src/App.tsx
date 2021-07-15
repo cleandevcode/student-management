f879e981e4d7ddc3ae4e56c7f0f401de5dde32df
DESKTOP-VVJ6TUI\blues <blueskyremi@outlook.com>
2021-07-15 22:22:15 +0800

    basic

--- src/App.tsx
+++ src/App.tsx
@@ -1,13 +1,15 @@
-import React from 'react';
+import * as React from 'react';
 import { BrowserRouter as Router, Switch, Route } from "react-router-dom";
 import 'bootstrap/dist/css/bootstrap.min.css'; 
-import './App.css';
+import './app.scss';
 
-function App() {
+import { Home, Detail } from "./pages"
+
+const App: React.FC = () => {
   return (
    <Router>
       <Switch>
-        <Route path="/" exact component={Home} />
+        <Route exact path="/" component={Home} />
         <Route path="/student/:id" component={Detail} />
       </Switch>
     </Router>
--- src/pages/Home/index.tsx
+++ src/pages/Home/index.tsx
@@ -1,5 +1,8 @@
 import React, { useEffect, useState } from "react";
 import { useSelector, useDispatch } from "react-redux";
+import BaseService from '../../service/base.service';
+import "../../app.scss";
+import { DataTable, CreateUser, UpdateUser, DeleteUser,  } from "../../components"
 
 export default function Home() {
     
