2c9315624af13d24c6da3dd4621f24c071ed50bd
DESKTOP-VVJ6TUI\blues <blueskyremi@outlook.com>
2021-07-18 23:03:03 +0800

    search

--- src/components/index.component.tsx
+++ src/components/index.component.tsx
@@ -9,30 +9,58 @@ import SortIcon from "../assets/sort-icon.png";
 interface IProps {}
 interface IState {
   listPersons: Array<Person>;
+  filteredPersons: Array<Person>;
   isReady: Boolean;
   hasError: Boolean;
   sorted: Boolean;
+  searchKey: string;
 }
 
 class Index extends React.Component<IProps, IState> {
   public state: IState = {
     listPersons: new Array<Person>(),
+    filteredPersons: new Array<Person>(),
     isReady: false,
     hasError: false,
     sorted: false,
+    searchKey: "",
   };
   constructor(props: IProps) {
     super(props);
     this.state = {
       isReady: false,
       listPersons: Array<Person>(),
+      filteredPersons: new Array<Person>(),
       hasError: false,
       sorted: false,
+      searchKey: "",
     };
     this.onSorting = this.onSorting.bind(this);
+    this.handleChange = this.handleChange.bind(this);
   }
 
   public componentDidMount() {
+    this.loadData();
+    setTimeout(() => {
+      if (!this.state.isReady) {
+        toastr.info(
+          "It is possible that the service is being restarted, please wait more ...",
+          "",
+          { timeOut: 8000 }
+        );
+      }
+
+      if (this.state.hasError) {
+        toastr.error("An error occurred!", "", { timeOut: 8000 });
+      }
+    }, 2000);
+  }
+
+  componentWillMount() {
+    this.loadData();
+  }
+
+  loadData() {
     BaseService.getAll<Person>("/students").then((rp) => {
       if (rp.status) {
         const data = rp.data;
@@ -51,7 +79,14 @@ class Index extends React.Component<IProps, IState> {
           );
         });
 
-        this.setState({ listPersons: listPersons });
+        this.setState(
+          {
+            listPersons: listPersons,
+          },
+          () => {
+            this.search(this.state.searchKey);
+          }
+        );
         this.setState({ isReady: true });
       } else {
         this.setState({ isReady: true });
@@ -59,20 +94,6 @@ class Index extends React.Component<IProps, IState> {
         console.log("Messages: " + rp.message);
       }
     });
-
-    setTimeout(() => {
-      if (!this.state.isReady) {
-        toastr.info(
-          "It is possible that the service is being restarted, please wait more ...",
-          "",
-          { timeOut: 8000 }
-        );
-      }
-
-      if (this.state.hasError) {
-        toastr.error("An error occurred!", "", { timeOut: 8000 });
-      }
-    }, 2000);
   }
 
   public tabRow = () => {
@@ -109,32 +130,65 @@ class Index extends React.Component<IProps, IState> {
       );
     }
 
-    return this.state.listPersons.map(function (object, i) {
+    return this.state.filteredPersons.map(function (object, i) {
       return <TableRow key={i} index={i + 1} person={object} />;
     });
   };
 
   onSorting(keyword: string, sorted: Boolean) {
-    const { listPersons } = this.state;
+    const { filteredPersons } = this.state;
 
     switch (keyword) {
       case keyword:
-        const result = listPersons.sort((a: any, b: any) => {
+        const result = filteredPersons.sort((a: any, b: any) => {
           return sorted
             ? a[keyword].localeCompare(b[keyword])
             : b[keyword].localeCompare(a[keyword]);
         });
-        this.setState({ ...this.state, listPersons: result, sorted: !sorted });
+        this.setState({
+          ...this.state,
+          filteredPersons: result,
+          sorted: !sorted,
+        });
         return;
       default:
         break;
     }
   }
 
+  search = (term: string) => {
+    let originalLists = this.state.listPersons;
+    const results = originalLists.filter((user: any) =>
+      Object.keys(user).some((key) =>
+        user[key]
+          .toString()
+          .toLowerCase()
+          .includes(term.toString().toLowerCase())
+      )
+    );
+    this.setState({ ...this.state, filteredPersons: results });
+  };
+
+  handleChange(event: any) {
+    console.log("test>>>>", event.target.value);
+    this.setState({ searchKey: event.target.value }, () => {
+      this.search(this.state.searchKey);
+    });
+  }
+
   public render(): React.ReactNode {
-    const { sorted } = this.state;
+    const { sorted, searchKey } = this.state;
     return (
       <div>
+        <div className="d-flex flex-end">
+          <input
+            type="text"
+            className="form-control mb-3"
+            value={searchKey}
+            onChange={this.handleChange}
+            placeholder="Enter the search key.."
+          />
+        </div>
         <h3 className="text-center">Persons List</h3>
         <table className="table table-striped" style={{ marginTop: 20 }}>
           <thead>
